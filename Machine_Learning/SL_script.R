872e4612522d57acc7dd7128a8c58d64983e958e
ellivromvvn <el_livro3485@yahoo.com>
2018-10-22 15:15:32 +0800

    day1 pm break

--- Machine_Learning/SL_script.R
+++ Machine_Learning/SL_script.R
@@ -35,3 +35,24 @@ predict(lm.fit, data.frame(lstat=c(5,10,15)),
 predict(lm.fit, data.frame(lstat=c(5,10,15)),
         interval="prediction")
 
+attach(Boston)
+plot(lstat,medv)
+abline(lm.fit,lwd=3,col="red")
+
+par(mfrow=c(2,2),mar=c(3,3,2,3))
+plot(lm.fit)
+
+par(mfrow=c(1,1))
+plot(predict(lm.fit),rstudent(lm.fit))
+
+plot(hatvalues(lm.fit)) #leverage
+which.max(hatvalues(lm.fit))
+
+lm.fit <- lm(medv~lstat+age)
+summary(lm.fit)
+
+lm.fit1<-lm(medv~., data=Boston)
+summary(lm.fit1)
+
+lm.fit2<-lm(medv~.-age, data = Boston)
+summary(lm.fit2)
