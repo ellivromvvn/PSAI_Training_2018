51e765359cd35e7d76e0790c11b4bcd095ba5028
ellivromvvn <el_livro3485@yahoo.com>
2018-10-22 17:33:37 +0800

    day1 done

--- Machine_Learning/SL_script.R
+++ Machine_Learning/SL_script.R
@@ -51,8 +51,50 @@ which.max(hatvalues(lm.fit))
 lm.fit <- lm(medv~lstat+age)
 summary(lm.fit)
 
-lm.fit1<-lm(medv~., data=Boston)
+lm.fit1<-lm(medv~., data=Boston) #all
 summary(lm.fit1)
 
-lm.fit2<-lm(medv~.-age, data = Boston)
+lm.fit2<-lm(medv~.-age, data = Boston) #except age
 summary(lm.fit2)
+
+lm.fit3 <- lm(medv~lstat*age, data = Boston)
+summary(lm.fit3)
+
+install.packages("olsrr")
+library(olsrr)
+
+full<-lm(medv~., data = Boston)
+k<-ols_step_best_subset(full)
+k
+plot(k)
+
+k<-ols_step_both_p(full)
+k
+plot(k)
+
+lm.fit<-lm(medv~lstat+age,data = Boston)
+par(mfrow=c(2,2))
+plot(lm.fit)
+
+lm.fit2<-lm(medv~lstat+I(lstat^2)+age,data=Boston)
+summary(lm.fit2)
+
+plot(lm.fit2)
+
+anova(lm.fit,lm.fit2) #which is the best fit model
+
+lm.fit3<-lm(medv~poly(lstat,5)+age,data=Boston)
+summary(lm.fit3)
+
+lm.fit4<-lm(medv~log(lstat)+age,data=Boston)
+summary(lm.fit4)
+
+data(Carseats)
+View(Carseats)
+?Carseats
+
+lm.fit<-lm(Sales~Income+Advertising+Price+Age+ShelveLoc,data=Carseats)
+summary(lm.fit)
+contrasts(Carseats$ShelveLoc) #to know baseline
+
+library(car)
